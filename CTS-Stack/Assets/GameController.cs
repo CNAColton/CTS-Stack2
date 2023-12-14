3b4f49cafc3766f089c04391a4fea691293182c8
CNAColton <CNAColton@gmail.com>
2023-12-14 17:36:12 -0500

    ttest

---
+++ CTS-Stack/Assets/GameController.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+//Current cube game object
+[Header("CubeObject")]
+public GameObjetc currentCube;
+//Last cube game object
+[Header("Last Cube Object")]
+public GameObject lastCube;
+//Text object
+[Header("Text object")]
+public Text text;
+//Level number interger
+[Header("Current Level")]
+public int Level;
+//Boolean determining if game
+//is over
+[Header("Boolean")]
+public bool Done;
+{
+    //User Interface namespace
+    using UnityEngine.UI;
+    //Scene Management namespace
+    using UnityEngine.SceneManagment;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Call New Block function
+        Newblock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+                
+    }
+    //New Block function to create new block
+    //for the game
+    void Newblock()
+    {
+        //If the last cube is not
+        //destroyed
+        if(lastCube != null)
+        {
+            //The current cube position equals to all 3 axis positions
+            //to the nearest interger
+            currentCube.transform.position = new Vector3(Mathf.Round(currentCube.transform.position.x),
+                currentCube.transform.position.y,
+                Mathf.Round(currentCube.transform.position.z));
+            //current cubes size equals to the last cube size
+            currentCube.transform.localScale = new Vector3(lastCube.transform.localScale.x - Mathf.Abs(currentCube.transform.position.x - lastCube.transform.position.x),
+                                                            lastCube.transform.localScale.y,
+                                                            lastCube.transform.localScale.z - Mathf.Abs(currentCube.transform.position.z - lastCube.transform.position.z));
+            //current cubes position equals to the current cubes x position
+            //last cubes y position
+            //z axis position of 0.5
+            currentCube.tranform.position = Vector3.Lerp(currentCube.transform.position, lastCube.transform.position, 0.5f) + Vector3.up * 5f;
+        }
+        //is less than or equal to 0 or if the
+        //current cube size on the z axis is less
+        //than or equal to 0
+        if (currentCube.transform.localScale.x <= 0f ||
+            currentCube.transform.localScale.z <= 0f)
+        {
+            //Done equals to true
+            Done = true;
+            //Text is visible
+            text.gameObject.SetAcive(true);
+            //Text equals to the text of the Final socre
+            //and which level is played
+            text.text. = "Final Score: " + Level;
+            //Start Corountine function X
+            StartCoroutine(X());
+            return;
+        }
+        //Last cube equals to the
+        //current cube
+        lastCube = currentCube;
+        //current cube equals to the spawned
+        //last cube
+        currentCube = Instantiate(lastCube);
+        //Current cubes name equals to the
+        //level number
+        currentCube.name = Level + "";
+        //Current cube component mesh renderer material
+        //set the color according to the color settings
+        currentCube.GetComponent<MeshRenderer>().material.SetColor("_Color", Color.HSVToRGB((Level / 100f) % 1f, 1f, 1f));
+        //Add 1 to level
+        Level++;
+        //Camera position equals to the
+        //position of the current cube
+        Camera.main.transform.position = currentCube.transform.position + new Vector3(100, 100, 100);
+        //Camera looks at the current cube
+        Camera.main.transform.LookAt(currentCube.transform.position);
+    }
+}
